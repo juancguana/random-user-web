5a169bf3af38202d01fb0b045fac21179a4cd09d
Juan Guana <juanc.guana@gmail.com>
2021-06-28 12:55:33 -0500

    preparar para el deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,20 +24,20 @@ const Header = () => {
     <AppBar position='sticky'>
       <Container maxWidth='lg'>
         <Toolbar className={classes.menu}>
-          <IconButton edge='start'>
+          <IconButton component={RouterLink} to="/" edge='start' onClick={()=> window.location.reload()}>
             <img src={Logo} alt='Logo' height='30px' />
           </IconButton>
+{/* 
           <Link component={RouterLink} to="/" color='inherit'>
             Inicio
           </Link>
-
           <Link component={RouterLink} to="/form" color='inherit'>
             Formulario
           </Link>
 
           <Link component={RouterLink} to="/list" color='inherit'>
             Lista
-          </Link>
+          </Link> */}
         </Toolbar>
       </Container>
     </AppBar>
